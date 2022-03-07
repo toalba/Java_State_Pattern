fd5a46c9a0c7f2224fcda115c4d5adb5a24f173c
DreamTecHunter <Tobias.laser@ymail.com>
2022-03-07 11:05:52 +0100

    State-class "CoinInsertedState" added

---
+++ src/CoinInsertedState.java
@@ -0,0 +1,16 @@
+public class CoinInsertedState extends State{
+    @Override
+    public void status() {
+
+    }
+
+    @Override
+    public void prev() {
+
+    }
+
+    @Override
+    public void next() {
+
+    }
+}
