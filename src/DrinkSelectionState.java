541141f65f93c33791da28a7a9bb66ea3208308b
DreamTecHunter <Tobias.laser@ymail.com>
2022-03-07 11:28:22 +0100

    DrinkSelectionState edited

--- src/DrinkSelectionState.java
+++ src/DrinkSelectionState.java
@@ -4,16 +4,16 @@
 public class DrinkSelectionState extends State{
     @Override
     public void status() {
-
+        System.out.println("Drink get selected.");
     }
 
     @Override
     public void prev() {
-
+        this.automate.setState(new CoinInsertedState());
     }
 
     @Override
     public void next() {
-
+        this.automate.setState(new DrinkOutputState());
     }
 }
