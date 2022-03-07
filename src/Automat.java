a6bb01523bcbf47e1108da0e9abbb0a7e98b6131
Baumbart13 <tkefer@tsn.at>
2022-03-07 10:54:36 +0100

    woas nid wos mit torben abgeht oida

--- src/Automat.java
+++ src/Automat.java
@@ -1,7 +1,19 @@
+import java.util.List;
+
 public class Automat {
     private int aktuellerZustand;
 
     public int aktuellerZustand() {
         return aktuellerZustand;
+    }
+
+    private List<State> zustaende;
+
+    public List<State> getZustaende() {
+        return zustaende.subList(0, zustaende.size());
+    }
+
+    public void addZustand(State s) {
+        zustaende.add(s);
     }
 }
